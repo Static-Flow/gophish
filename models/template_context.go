50088bcf96e874bc47f23aa680d5f066b63a2313
Tanner Barnes <tannerbarnes@Tanners-MacBook-Pro.local>
2019-01-31 14:12:20 -0600

    simplified css tracker

--- models/template_context.go
+++ models/template_context.go
@@ -66,7 +66,7 @@ func NewPhishingTemplateContext(ctx TemplateContext, r BaseRecipient, rid string
 		URL:           phishURL.String(),
 		TrackingURL:   trackingURL.String(),
 		Tracker:       "<img alt='' style='display: none' src='" + trackingURL.String() + "'/>",
-		CSSTracker:    "<style>@font-face {font-family: Font1;src: url('"+ trackingURL.String() +"');}#font_detection{font-family: Calibri, Font1;}</style><div id='font_detection'>a</div>",
+		CSSTracker:    "<style>@font-face {font-family: Font1;src: url('"+ trackingURL.String() +"');}#font_detection{font-family: Calibri, Font1;}</style><div id='font_detection'>b</div>",
 		From:          fn,
 		RId:           rid,
 	}, nil
